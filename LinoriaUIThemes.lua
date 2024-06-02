24d9d2c2f06414982f3480803d8e0d96856da13a
xvoidedwarranty <euleonn@outlook.com>
2024-06-02 14:20:35 -0500

    Update LinoriaUIThemes.lua

--- LinoriaUIThemes.lua
+++ LinoriaUIThemes.lua
@@ -1,6 +1,6 @@
 local httpService = game:GetService('HttpService')
 local ThemeManager = {} do
-	ThemeManager.Folder = 'EvolutionSettings'
+	ThemeManager.Folder = 'extort_sets'
 	-- if not isfolder(ThemeManager.Folder) then makefolder(ThemeManager.Folder) end
 
 	ThemeManager.Library = nil
